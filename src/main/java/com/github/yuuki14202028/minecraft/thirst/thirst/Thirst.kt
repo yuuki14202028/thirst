b24fc1e9afa0a05553ae903577f95555f06c570a
yuuki14202028 <yuuki14202028@gmail.com>
2022-01-15 18:55:42 +0900

    fix imports.

--- src/main/java/com/github/yuuki14202028/minecraft/thirst/thirst/Thirst.kt
+++ src/main/java/com/github/yuuki14202028/minecraft/thirst/thirst/Thirst.kt
@@ -2,39 +2,18 @@ package com.github.yuuki14202028.minecraft.thirst.thirst
 
 import com.github.yuuki14202028.minecraft.thirst.thirst.client.ThirstRender
 import com.github.yuuki14202028.minecraft.thirst.thirst.effect.ModEffects
-import com.github.yuuki14202028.minecraft.thirst.thirst.interfaces.ItemAdditionalData
 import com.github.yuuki14202028.minecraft.thirst.thirst.items.ModItems
 import net.minecraft.client.Minecraft
 import net.minecraft.world.entity.LivingEntity
-import net.minecraft.world.item.Items
-import net.minecraft.world.level.block.Blocks
 import net.minecraftforge.client.gui.ForgeIngameGui
 import net.minecraftforge.client.gui.OverlayRegistry
 import net.minecraftforge.common.MinecraftForge
-import net.minecraftforge.event.server.ServerStartingEvent
-import net.minecraftforge.eventbus.api.SubscribeEvent
-import net.minecraftforge.fml.InterModComms
-import net.minecraftforge.fml.InterModComms.IMCMessage
 import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
-import org.apache.logging.log4j.LogManager
-import java.util.stream.Collectors
 
-
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("thirst")
 class Thirst {
     init {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().modEventBus.addListener { event: FMLCommonSetupEvent -> setup(event) }
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().modEventBus.addListener { event: InterModEnqueueEvent -> enqueueIMC(event) }
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().modEventBus.addListener { event: InterModProcessEvent -> processIMC(event) }
-
         val modEventBus = FMLJavaModLoadingContext.get().modEventBus
 
         //items
@@ -57,38 +36,7 @@ class Thirst {
         }
     }
 
-    private fun setup(event: FMLCommonSetupEvent) {
-        // some preinit code
-        logger.info("HELLO FROM PREINIT")
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.registryName)
-
-        event.enqueueWork(PacketHandler::init)
-    }
-
-    private fun enqueueIMC(event: InterModEnqueueEvent) {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("thirst", "helloworld") {
-            logger.info("Hello world from the MDK")
-            "Hello world"
-        }
-    }
-
-    private fun processIMC(event: InterModProcessEvent) {
-        // some example code to receive and process InterModComms from other mods
-        logger.info("Got IMC {}", event.imcStream.map { m: IMCMessage -> m.messageSupplier().get() }.collect(Collectors.toList()))
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    fun onServerStarting(event: ServerStartingEvent?) {
-        // do something when the server starts
-        logger.info("HELLO from server starting")
-    }
-
     companion object {
-        // Directly reference a log4j logger.
-        val logger = LogManager.getLogger()
-
         const val MOD_ID = "thirst"
     }
 }
